142b37dc281b176165d23ff38be5140b115e3914
francislazaro <94938457+francislazaro@users.noreply.github.com>
2026-03-19 15:56:25 -0400

    Add IQuittable interface with Quit method
    Define IQuittable interface with Quit method.

---
+++ IQuittable.cs
@@ -0,0 +1,7 @@
+// This interface defines a method called Quit()
+// Any class that uses this interface must implement this method
+public interface IQuittable
+{
+    // Method signature (no body here)
+    void Quit();
+}
